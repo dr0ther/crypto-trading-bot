7c6e5e9240993b4a06d40070f6bf02df909c6e9f
dr0ther <john.thomas.wade@gmail.com>
2021-07-19 19:51:19 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,10 +3,10 @@ dir1=/app
 dir2=/usr/src/app
 
 echo "list /app directory"
-ls $dir1
+ls -l $dir1
 
 echo "list /usr/src/app directory"
-ls $dir2
+ls -l $dir2
 
 for FILE in $(diff $dir1 $dir2|grep $dir1 | cut -f4 -d' ')
 do
