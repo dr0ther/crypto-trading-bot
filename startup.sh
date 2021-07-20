ce723f60af60785a8e4eab9df5c5d9a8b037a50b
dr0ther <john.thomas.wade@gmail.com>
2021-07-19 21:27:10 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -17,5 +17,16 @@ else
 fi
 
 
+FILE=/config/conf.json
+if [ -f "$FILE" ]; then
+    echo "$FILE exist"
+    rm -rf /usr/src/app/conf.json
+    cp /config/conf.json /usr/src/app/conf.json
+else 
+    echo "$FILE does not exist"
+    cp /usr/src/app/conf.json /config/conf.json
+fi
+
+
 #keep it running
 tail -f /dev/null
