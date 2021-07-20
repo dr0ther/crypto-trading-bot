299390b9004f2b68f7b106472f2226fbb7ac544a
dr0ther <john.thomas.wade@gmail.com>
2021-07-19 22:24:25 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-source=/app
-dest=/usr/src/app
-
-
-
-#rsync -arv $source/* $dest/ 
 
 FILE=/usr/src/app/conf.json
 if [ -f "$FILE" ]; then
     echo "$FILE exists, do nothing"
 else 
     echo "$FILE does not exist"
-    cp -pr /temp/* /usr/src/app
+    git clone https://github.com/dr0ther/crypto-trading-bot.git /usr/src/app
+    cp /usr/src/app/instance.js.dist /usr/src/app/instance.js 
+    cp /usr/src/app/conf.json.dist /usr/src/app/conf.json 
+    sqlite3 bot.db < /usr/src/app/bot.sql
+
+
+    
 fi
 
 
