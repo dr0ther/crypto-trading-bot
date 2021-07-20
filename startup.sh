0e838eeea21589e1495e30a3dc151aed711e69f6
dr0ther <john.thomas.wade@gmail.com>
2021-07-19 21:42:42 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,26 +6,15 @@ dest=/usr/src/app
 
 #rsync -arv $source/* $dest/ 
 
-FILE=/config/conf.json
+FILE=/usr/src/app/conf.json
 if [ -f "$FILE" ]; then
-    echo "$FILE exist"
-    rm -rf /usr/src/app/conf.json
-    cp /config/conf.json /usr/src/app/conf.json
+    echo "$FILE exists, do nothing"
 else 
     echo "$FILE does not exist"
-    cp /usr/src/app/conf.json /config/conf.json
+    git clone https://github.com/dr0ther/crypto-trading-bot.git /usr/src/app
 fi
 
 
-FILE=/config/conf.json
-if [ -f "$FILE" ]; then
-    echo "$FILE exist"
-    rm -rf /usr/src/app/conf.json
-    cp /config/conf.json /usr/src/app/conf.json
-else 
-    echo "$FILE does not exist"
-    cp /usr/src/app/conf.json /config/conf.json
-fi
 
 
 #keep it running
